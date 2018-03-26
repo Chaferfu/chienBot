86bab8a0ea4a13cce5250d186f7bbd72f60c57d3
Chaferfu <mathba@gmx.com>
2018-03-26 14:38:50 +0200

    Chien bot est pret

--- chienBot.py
+++ chienBot.py
@@ -1,3 +1,7 @@
+from random import randint, uniform
+from time import sleep
+
+
 repliques = []
 repliques.append("Waf!")
 repliques.append("Wuf!")
@@ -7,3 +11,13 @@ repliques.append("Wof!")
 repliques.append("Wouf!")
 repliques.append("Wigrecf!")
   
+while True:
+
+	
+	text = input("Moi    : ")
+	sleep(uniform(0.5,1.5))
+	print("Toutou : " + repliques[randint(0,6)])
+
+#Renvoie une reponse plutot 
+def reponseNulle():
+	return
