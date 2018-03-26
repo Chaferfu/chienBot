4f659c3a9481958469987dc93062dd38273e334c
Chaferfu <mathba@gmx.com>
2018-03-26 14:44:42 +0200

    nathan fait des conflits

--- chienBot.py
+++ chienBot.py
@@ -1,23 +1,30 @@
 from random import randint, uniform
 from time import sleep
 
+#Renvoie une reponse plutot nulle et non constructive
+def reponseNulle(tabMots):
+
+	rng = randint(1,2)
+	text = ""
+
+	for i in range(rng):
+		text += tabMots[randint(0,len(tabMots) - 1)]
+
+	return text
 
 repliques = []
-repliques.append("Waf!")
-repliques.append("Wuf!")
-repliques.append("Wef!")
-repliques.append("Wif!")
-repliques.append("Wof!")
-repliques.append("Wouf!")
-repliques.append("Wigrecf!")
+repliques.append("Waf! ")
+repliques.append("Wuf! ")
+repliques.append("Wef! ")
+repliques.append("Wif! ")
+repliques.append("Wof! ")
+repliques.append("Wouf! ")
+repliques.append("Wigrecf! ")
   
 while True:
 
 	
 	text = input("Moi    : ")
 	sleep(uniform(0.5,1.5))
-	print("Toutou : " + repliques[randint(0,6)])
+	print("Toutou : " + reponseNulle(repliques))
 
-#Renvoie une reponse plutot 
-def reponseNulle():
-	return
