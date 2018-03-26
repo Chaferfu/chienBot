c897162e70f090f66b65e83eb55eaf62f0357e8a
Chaferfu <mathba@gmx.com>
2018-03-26 14:00:19 +0200

    les repliques sont la

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # chienBot
+#salut
---
+++ chienBot.py
@@ -0,0 +1,8 @@
+repliques = []
+repliques.append("Waf!")
+repliques.append("Wuf!")
+repliques.append("Wef!")
+repliques.append("Wif!")
+repliques.append("Wof!")
+repliques.append("Wouf!")
+repliques.append("Wigrecf!")
