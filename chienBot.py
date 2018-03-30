1f223d9f359553bb393a89cad237fee51e6dc0d5
Nathan Bonnard <nbonna1@a-018818.ups.u-psud.fr>
2018-03-30 15:31:41 +0200

    je suis encore entrain de travailler, je fais que push, y a vraiment que moi qui bosse sur ce projet

--- chienBot.py
+++ chienBot.py
@@ -51,28 +51,27 @@ def read_word_list_file(filename):
 
 #renvoie les mots & réponses contenues dans le fichier
 def stockWordsAndQuestions(filename):
-    dictThemes = {}
-    
-    with open(filename, "r") as filepointer:
-        for line in file.readlines():
-        	theme
-			mots = []
-			questions = []
-			while line != '@':
-            	if line == '£':
-                	line = file.readlines()
-                    theme = line
-                else:
-                    mots.append(line)
-                line = file.readlines()
-
-            line = file.readlines()
-            while line != '$':
-                questions.append(line)
-                line = file.readlines()
-            dictThemes[theme] = (mots, questions)
-
-    return dictThemes
+	dictThemes = {}
+	key = ""
+	theme = ""
+	mots = []
+	questions = []
+	with open(filename, "r") as filepointer:
+		for line in filepointer:
+			line = line.strip()
+			if line[0] in ['£','@']:
+				key = line[0]
+				if key == '£':
+					if theme != "":
+						mots.pop()
+						questions.pop()
+						dictThemes[theme] = (mots, questions)
+					theme = line[1:]				
+			elif(key == '£'):
+				mots.append(line)
+			elif(key == '@'):
+				questions.append(line)
+	return dictThemes
 
 def mode1():
 	print("mode 1")
@@ -96,7 +95,6 @@ def reponseNulle(tabMots):
 
 	rng = randint(1,3)
 	text = ""
-
 	for i in range(rng):
 		text += tabMots[randint(0,len(tabMots) - 1)]
 		text += " "
@@ -105,22 +103,24 @@ def reponseNulle(tabMots):
 
 if __name__=="__main__":
 	d = stockWordsAndQuestions("mode2")
-	for x in d:
-		print (x)
-		for y in d[x]
-			print(y, ':', d[x][y])
-			
-	while(int(mode) != 4):
-		while ((int(mode) < 0) or (int(mode) > 4)): 
-			mode = input("Choisissez un mode entre 0, 1, 2 et 3 (4 pour quitter) ");
-		if(int(mode) == 0):
-			mathias();
-		elif(int(mode) == 1):
-			mode1();
-		elif(int(mode) == 2):
-			mode2();
-		elif(int(mode) == 3):
-			mode3();
-		else:
-			break;
-		mode = "-1"
+	for theme,valeur in d.items():
+		print(theme, " :")
+		for mot in valeur[0]:
+			print("mot :",mot)
+		for question in valeur[1]:
+			print("question :",question)
+
+	# while(int(mode) != 4):
+	# 	while ((int(mode) < 0) or (int(mode) > 4)): 
+	# 		mode = input("Choisissez un mode entre 0, 1, 2 et 3 (4 pour quitter) ");
+	# 	if(int(mode) == 0):
+	# 		mathias();
+	# 	elif(int(mode) == 1):
+	# 		mode1();
+	# 	elif(int(mode) == 2):
+	# 		mode2();
+	# 	elif(int(mode) == 3):
+	# 		mode3();
+	# 	else:
+	# 		break;
+	# 	mode = "-1"
