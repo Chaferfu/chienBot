5fe156ba2b90c76309fd0fd76df69856212404d8
meakitfed <nathan.bonnard@hotmail.fr>
2018-04-23 12:15:11 +0200

    nathan gouts

--- user.py
+++ user.py
@@ -11,7 +11,7 @@ class User:
 		# sports :
 		self.sport = []
 
-		# gouts :
+		# gouts [Like - Dislike]:
 		self.gouts = [[], []]
 
 		# relations :
@@ -24,16 +24,28 @@ class User:
 		self.anecdotes = []
 
 	# ajoute un gouts
-	def addGouts(self,k,b):
-		return
+	def addDislike(self,k,b):
+		if b not in self.gouts[1] and b != '':
+			self.gouts[1].append(b)
+
+	# ajoute un gouts
+	def addLike(self,k,b):
+		if b not in self.gouts[0] and b != '':
+			self.gouts[0].append(b)
 
 	# permet d'afficher les informations sotckées sur le gouts
 	def printGoutsUser(self):
-		return
+		print("Goûts  : ")
+		print("Like : ")
+		for k in self.gouts[0]:	
+			print("\t-" + k.lower())
+		print("Dislike : ")
+		for k in self.gouts[1]:	
+			print("\t-" + k.lower())	
 
 	# ajoute un sport
 	def addSport(self,sport):
-		if sport not in self.sport:
+		if sport not in self.sport and sport != '':
 			self.sport.append(sport)
 
 	# permet d'afficher les informations sotckées sur le sport
@@ -42,7 +54,6 @@ class User:
 		for k in self.sport:	
 			print("\t-" + k.lower())
 			
-		print("")
 
 	# ajoute un membre de la famille en fonction d'une clé k (ex:Tante) et d'une valeur v (ex:George).
 	# si un des deux est nul, il n'y a pas d'ajout.
@@ -63,7 +74,6 @@ class User:
 			for name in self.famille[k]:
 				print("\t-" + name.lower())
 
-		print("")
 	# ajoute une relation en fonction d'une clé k (ex:Tante) et d'une valeur v (ex:George).
 	# si un des deux est nul, il n'y a pas d'ajout.
 	def addRelation(self,k,v):
@@ -85,18 +95,10 @@ class User:
 
 		print("")
 
-	def changeState(self, val):
-		self.etat += val
-
-	def printState(self):
-		if self.etat > 0:
-			print("PPR")
-		else:
-			print("VNR")
-
 	# permet d'afficher l'ensemble des informations stockées
 	def printInformationUser(self):
 		print("Information User ", self.infos[0])
 		self.printFamilyUser()
 		self.printSportUser()
 		self.printRelationUser()
+		self.printGoutsUser()
